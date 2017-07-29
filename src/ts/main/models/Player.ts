64f6c91a9d15c6fa58b34281e7ccaa61a0b673c9
hiradimir <yutaka@hiradimir.com>
2017-07-29 12:04:17 +0900

    Extends model of not contains belogTo

--- src/ts/main/models/Player.ts
+++ src/ts/main/models/Player.ts
@@ -1,8 +1,10 @@
 import {BelongsTo, Column, ForeignKey, Model, Table} from "sequelize-typescript";
 import Team from "./Team";
 import UserOperateModel from "../core/model/UserOperateModel";
+import BasicModel from "../core/model/BasicModel";
+
 @Table
-export default class Player extends UserOperateModel<Player> {
+export default class Player extends BasicModel<Player> {
 
   @Column
   name: string;
